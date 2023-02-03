ebd52a9d471378a6afdc2d926c40c57f5ac58868
Wilder Fulford <fulford.wilder@gmail.com>
2023-02-02 21:14:53 -0800

    Cleaned up RM loss file

--- src/superhf/reward_models/preference_losses.py
+++ src/superhf/reward_models/preference_losses.py
@@ -1,42 +1,37 @@
-import torch 
-import torch.nn as nn
+"""
+This file implements the loss function for our reward model.
+"""
 
-class PreferenceCrossEntropyLoss(nn.Module):
-    def __init__(self, weight = None, reduction = 'mean'):
-        super().__init__()
-        self.weight = weight        # could be useful for quality feedback
-        self.reduction = reduction 
-        
-    def forward(self, winner, loser):
-        """
+import torch
+from torch import nn
+
+
+class PreferenceLoss(nn.Module):
+    """
+    This loss function computes the negative-log likelihood (NLL) of correctly
+    predicting the winning input sequence out of a winner-loser pair.
+
+    The probability of predicting the winner is defined as:
+        sigmoid(reward(winner) - reward(loser))
+
+    `forward`:
         winner: size (batchsize,) tensor of reward scores of winning responses
         loser: size (batchsize,) tensor of reward scores of losing responses
-        """
-        
+        Return: mean or summed NLL loss.
+    """
+
+    def __init__(self, weight=None, reduction="mean"):
+        super().__init__()
+        self.weight = weight  # could be useful for quality feedback
+        self.reduction = reduction
+
+    def forward(self, winner, loser):
         weighted_probabilities = nn.Sigmoid(winner - loser)
         if self.weight is not None:
             weighted_probabilities *= self.weight
-            
-        loss = torch.log(weighted_probabilities) 
-        if self.reduction == 'mean':
+
+        loss = -torch.log(weighted_probabilities)
+        if self.reduction == "mean":
             return loss.mean()
-        else:
-            return loss.sum()
-        
-class PreferenceHingeLoss(nn.Module):
-    def __init__(self, reduction = 'mean'):
-        super().__init__()
-        self.reduction = reduction 
-    
-    def forward(self, winner, loser):
-        """
-        winner: size (batchsize,) tensor of reward scores of winning responses
-        loser: size (batchsize,) tensor of reward scores of losing responses
-        """
-        loss = 1 - (winner - loser)
-        loss[loss < 0] = 0
-        if self.reduction == 'mean':
-            return loss.mean()
-        else:
-            return loss.sum()
-        
+
+        return loss.sum()
