707e72d7582301bb674da1aff5c7f5cf91c7abab
Wilder Fulford <fulford.wilder@gmail.com>
2023-02-03 23:20:40 -0800

    Wrote RewardModel Class

--- src/superhf/reward_models/reward_model.py
+++ src/superhf/reward_models/reward_model.py
@@ -21,14 +21,20 @@ class PreferenceLoss(nn.Module):
         Return: mean NLL loss.
     """
 
-    def forward(
-        self,
-        winner_scores: torch.Tensor,
-        loser_scores: torch.Tensor,
-    ) -> float:
+    def forward(self, winner_scores: torch.Tensor, loser_scores):
         loss = -F.logsigmoid(winner_scores - loser_scores)
-        
+
         return loss.mean()
+
+
+def compute_metrics(eval_prediction):
+    """
+    Computes decimal accuracy of predictions during model evaluation.
+    """
+
+    reward_scores = eval_prediction.prediction
+    correct_predictions = (reward_scores[:, 0] - reward_scores[:, 1]) > 0
+    return correct_predictions.sum() / correct_predictions.size[0]
 
 
 class RewardModelTrainer(Trainer):
@@ -42,21 +48,46 @@ class RewardModelTrainer(Trainer):
         """
         model : RewardModel for computing loss
         inputs :
-            dict with keys 'prompts', 'winners', 'losers' each mapping to
-            a tokenized batch of inputs to pass to model.
+            input batch with features ['winner', 'loser'] to pass to model.
         Returns
         -------
         loss: model's loss over inputs, computed by the PreferenceLoss class.
-        outputs: size (batch, 2) tensor of reward scores, with winner_scores in
-            first column and loser_scores in second.
+        reward_scores: size (batch, 2) tensor of reward scores,
+            with winner_scores in first column and loser_scores in second.
 
         """
 
-        winner_scores = model(inputs["prompts"], inputs["winners"])
-        loser_scores = model(inputs["prompts"], inputs["losers"])
+        winner_scores = model(inputs["winner"])
+        loser_scores = model(inputs["loser"])
 
         loss_fct = PreferenceLoss()
         loss = loss_fct(winner_scores, loser_scores)
-        outputs = torch.stack([winner_scores, loser_scores], 1)
+        reward_scores = torch.stack([winner_scores, loser_scores], 1)
 
-        return loss, outputs if return_outputs else loss
+        return (loss, reward_scores) if return_outputs else loss
+
+    def prediction_step(model, inputs, prediction_loss_only, ignore_keys=None):
+        """
+        model : RewardModel for computing loss
+        inputs :
+            input batch with features ['winner', 'loser'] to pass to model.
+        Returns
+        -------
+        loss: model's loss over inputs, computed by the PreferenceLoss class.
+        reward_scores: size (batch, 2) tensor of reward scores,
+            with winner_scores in first column and loser_scores in second.
+        """
+        loss, reward_scores = self.compute_loss(model, inputs, True)
+        return (loss, None) if prediction_loss_only else (loss, reward_scores)
+
+
+class RewardModel(nn.Module):
+    """
+    The reward model to be trained from a pretrained language model.
+    """
+
+    def __init__(self, model_name, frozen_layers=()):
+        self.model = AutoModelForSequenceClassification(model_name, num_labels=1)
+        for name, param in self.model.base_model.named_parameters():
+            if name in frozen_layers:
+                param.requires_grad = False
