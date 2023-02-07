f902aa0ec1c0e8f06104c21a237f023e7f25a53e
Gabriel Mukobi <gabrielmukobi@gmail.com>
2023-02-07 14:11:43 -0800

    Get finetuning working

--- src/superhf/finetuning.py
+++ src/superhf/finetuning.py
@@ -13,15 +13,17 @@ from torch import nn
 import numpy as np
 from tqdm import tqdm
 from transformers import (
-    # Trainer,
+    Trainer,
     TrainingArguments,
-    # DataCollatorForLanguageModeling,
+    DataCollatorForLanguageModeling,
     PreTrainedModel,
     PreTrainedTokenizerBase,
     pipeline,
+    EvalPrediction,
 )
 from transformers.pipelines.pt_utils import KeyDataset
 from datasets.arrow_dataset import Dataset
+from datasets.utils import logging
 
 # from torchtyping import TensorType
 
@@ -72,7 +74,7 @@ class SinglePassBestOfNTrainer(SuperHFTrainer):
     and fine-tuning the language model on the filtered completions.
 
     As one long training sequence
-        1. Use $M$ to generate $n$ completions for each of $d$ training dataset prompts
+        1. Use $M$ to generate $n$ completions for each of $d$ training train_dataset prompts
               ($d*completions_per_prompt$ total).
         2. Use $R$ to select the top 1 of the $n$ completions for each prompt ($d$ total).
         3. Fine-tune $M$ on the $d$ best-of-$n$ completions.
@@ -102,6 +104,8 @@ class SinglePassBestOfNTrainer(SuperHFTrainer):
         self.temperature = temperature
         self.completions_per_prompt = completions_per_prompt
         self.output_dir = output_dir
+        self.training_args: Any = None
+        self.eval_dataset: Any = None
         # Make output dir if it doesn't exist
         if not os.path.exists(self.output_dir):
             os.makedirs(self.output_dir)
@@ -113,7 +117,7 @@ class SinglePassBestOfNTrainer(SuperHFTrainer):
 
     def generate_completions(self, batch_size: int, max_new_tokens: int) -> None:
         """
-        Use $M$ to generate $n$ completions for each of $d$ training dataset prompts.
+        Use $M$ to generate $n$ completions for each of $d$ training train_dataset prompts.
         """
         # Set up tokenizer
         if self.language_tokenizer.pad_token is None:
@@ -128,10 +132,10 @@ class SinglePassBestOfNTrainer(SuperHFTrainer):
         # Duplicate each of the prompts $n$ times.
         prompts = self.train_prompts * self.completions_per_prompt
 
-        # Convert prompts into a dataset
-        dataset = Dataset.from_dict({"prompt": prompts})
+        # Convert prompts into a train_dataset
+        train_dataset = Dataset.from_dict({"prompt": prompts})
 
-        # Use $M$ to generate $n$ completions for each of $d$ training dataset prompts
+        # Use $M$ to generate $n$ completions for each of $d$ training train_dataset prompts
         # ($d*completions_per_prompt$ total). Iterate in groups of batch_size.
         pipe = pipeline(
             "text-generation",
@@ -143,10 +147,10 @@ class SinglePassBestOfNTrainer(SuperHFTrainer):
         print("Generating completions...")
         completions: List[str] = []
 
-        # for out in pipe(KeyDataset(dataset, "prompt")):
+        # for out in pipe(KeyDataset(train_dataset, "prompt")):
         for out in tqdm(
             pipe(
-                KeyDataset(dataset, "prompt"),
+                KeyDataset(train_dataset, "prompt"),
                 batch_size=batch_size,
                 max_new_tokens=max_new_tokens,
                 temperature=self.temperature,
@@ -154,7 +158,7 @@ class SinglePassBestOfNTrainer(SuperHFTrainer):
                 early_stopping=True,
                 do_sample=True,
             ),
-            total=len(dataset),
+            total=len(train_dataset),
         ):
             completion = out[0]["generated_text"]
             # Filter out everything including and after the second "\n\nHuman:"
@@ -199,7 +203,7 @@ class SinglePassBestOfNTrainer(SuperHFTrainer):
             f"Loaded {new_size} completions (filtered {old_size - new_size} from {old_size} total)."
         )
 
-        dataset = Dataset.from_dict({"completion": completions})
+        train_dataset = Dataset.from_dict({"completion": completions})
 
         # Use $R$ to select the top 1 of the $n$ completions for each prompt ($d$ total).
         pipe = pipeline(
@@ -213,11 +217,11 @@ class SinglePassBestOfNTrainer(SuperHFTrainer):
         for row, completion in zip(
             tqdm(
                 pipe(
-                    KeyDataset(dataset, "completion"),
+                    KeyDataset(train_dataset, "completion"),
                     batch_size=batch_size,
                     max_length=512,
                 ),
-                total=len(dataset),
+                total=len(train_dataset),
             ),
             completions,
         ):
@@ -260,19 +264,112 @@ class SinglePassBestOfNTrainer(SuperHFTrainer):
         )
         return scored_completions, filtered_completions
 
-    def tune_model(self) -> None:
+    def tune_model(self, training_args: TrainingArguments) -> None:
         """
         Fine-tune $M$ on the $d$ best-of-$n$ completions.
         Evaluate the loss and average reward during training.
         """
+        self.training_args = training_args
         filtered_completions = torch.load(
             os.path.join(self.output_dir, "filtered_completions.pt")
         )
         print(f"Loaded {len(filtered_completions)} filtered completions")
-        for completion in filtered_completions[:2]:
-            print(
-                f'Score: {completion["score"]}, Completion: {completion["completion"]}'
-            )
+
+        train_dataset = Dataset.from_dict({"completion": filtered_completions})
+        eval_dataset = Dataset.from_dict({"prompt": self.test_prompts})
+        self.eval_dataset = eval_dataset
+
+        print("Pre-processing datasets...")
+        logging.enable_progress_bar()
+        if self.language_tokenizer.pad_token is None:
+            self.language_tokenizer.pad_token = self.language_tokenizer.eos_token
+
+        train_dataset_processed = train_dataset.map(
+            lambda examples: self.language_tokenizer(
+                [example["completion"] for example in examples["completion"]],
+                truncation=True,
+            ),
+            batched=True,
+        )
+        test_dataset_processed = eval_dataset.map(
+            lambda examples: self.language_tokenizer(
+                list(examples["prompt"]),
+                truncation=True,
+            ),
+            batched=True,
+        )
+
+        print("Beginning training...")
+        data_collator = DataCollatorForLanguageModeling(
+            tokenizer=self.language_tokenizer, mlm=False
+        )
+        self.language_model.train()
+        trainer = Trainer(
+            model=self.language_model,
+            data_collator=data_collator,
+            args=training_args,
+            train_dataset=train_dataset_processed,
+            eval_dataset=test_dataset_processed,
+            compute_metrics=self.compute_metrics,
+        )
+        trainer.train()
+
+    def compute_metrics(self, _: EvalPrediction) -> Dict[str, float]:
+        """
+        Compute the average reward of new completions on the test prompts.
+
+        We ignore the predictions and labels because we need to generate full
+        completions for the test prompts, which is not possible with the
+        Trainer API.
+        """
+        pipe = pipeline(
+            "text-generation",
+            model=self.language_model,
+            tokenizer=self.language_tokenizer,
+            device=self.language_model.device,
+        )
+
+        completions: List[str] = []
+        for out in pipe(
+            KeyDataset(self.eval_dataset, "prompt"),
+            batch_size=self.training_args.eval_batch_size,
+            max_new_tokens=256,
+            # temperature=self.temperature,
+            pad_token_id=self.language_tokenizer.pad_token_id,
+            early_stopping=True,
+            do_sample=False,
+        ):
+            completion = out[0]["generated_text"]
+            # Filter out everything including and after the second "\n\nHuman:"
+            # sequence in case the model repeats the prompt
+            completion = "\n\nHuman:".join(completion.split("\n\nHuman:")[:2])
+            completion = "\n\nAssistant:".join(completion.split("\n\nAssistant:")[:2])
+            completions.append(completion)
+
+        # Now evaluate the completions with the reward model
+        completions_dataset = Dataset.from_dict({"completion": completions})
+
+        # Use $R$ to select the top 1 of the $n$ completions for each prompt ($d$ total).
+        pipe = pipeline(
+            "text-classification",
+            model=self.reward_model,
+            tokenizer=self.reward_tokenizer,
+            device=self.reward_model.device,
+        )
+        scores: List[float] = []
+        print("Scoring completions...")
+        for row, completion in zip(
+            pipe(
+                KeyDataset(completions_dataset, "completion"),
+                batch_size=self.training_args.eval_batch_size,
+                max_length=512,
+            ),
+            completions,
+        ):
+            scores.append(row["score"])
+
+        average_reward = float(np.mean(scores))
+        return {"average_reward": average_reward}
 
 
 # class IterativeBestOfNTrainer(SuperHFTrainer):
