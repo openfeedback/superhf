f2b3c88d87853ced2cc7759ddb1e38f0c0184e21
Gabriel Mukobi <gabrielmukobi@gmail.com>
2023-03-17 21:30:11 -0700

    Don't split single quotes because of contractions

--- src/superhf/constants.py
+++ src/superhf/constants.py
@@ -8,5 +8,5 @@ PROMPT_DELIMITER = "\n\nAssistant:"
 # Matches "\n\n{anything}:" for pruning additional generated turns
 PROMPT_DELIMITER_REGEX_SIMPLE = re.compile(r"\n\n[^:]+:")
 
-# New delimiter: the above, but also "\n", single/double quotes, "-", "Human", and "Assistant"
-PROMPT_DELIMITER_REGEX_COMPLEX = re.compile(r"\n\n[^:]+:|\n|'|\"|-|Human|Assistant")
+# New delimiter: the above, but also "\n", double quotes, "-", "Human", and "Assistant"
+PROMPT_DELIMITER_REGEX_COMPLEX = re.compile(r"\n\n[^:]+:|\n|\"|-|Human|Assistant")
