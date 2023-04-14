4a1814161021e0b2a1215c5da1fd51e2225e7c09
RWindesheim <104098243+RWindesheim@users.noreply.github.com>
2023-04-14 22:45:12 +0200

    Update WebGPTComparisons_dataset.py

--- src/reward_modelling/WebGPTComparisons_dataset.py
+++ src/reward_modelling/WebGPTComparisons_dataset.py
@@ -6,23 +6,25 @@ from datasets import load_dataset
 class WebGPTComparisons(Dataset):
     def __init__(self, split="train"):
         data = load_dataset("openai/webgpt_comparisons", split=split)
-        self.filtered_responses = [
-            (self.remove_reference_numbers(example[f'answer_{winner_idx}']),
-             self.remove_reference_numbers(example[f'answer_{loser_idx}']))
-            for example in data
-            if example['score_0'] != example['score_1']
-            for winner_idx, loser_idx in [(0, 1) if example['score_0'] > example['score_1'] else (1, 0)]
-        ]
+        self.questions = []
+        self.winner_responses = []
+        self.loser_responses = []
+
+        for example in data:
+            if example['score_0'] != example['score_1']:
+                winner_idx, loser_idx = (0, 1) if example['score_0'] > example['score_1'] else (1, 0)
+                self.questions.append(example['question']['full_text'])
+                self.winner_responses.append(self.remove_reference_numbers(example[f'answer_{winner_idx}']))
+                self.loser_responses.append(self.remove_reference_numbers(example[f'answer_{loser_idx}']))
 
     @staticmethod
     def remove_reference_numbers(text):
         return re.sub(r'\[\d+\]', '', text)
 
     def __getitem__(self, idx):
-        return self.filtered_responses[idx]
+        return self.questions[idx], self.winner_responses[idx], self.loser_responses[idx]
 
     def __len__(self):
-        return len(self.filtered_responses)
-
+        return len(self.winner_responses)
 
 
