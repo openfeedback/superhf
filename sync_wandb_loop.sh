8ecf06f9bd86bc263eba4d76a14df07c4dd3b4ef
Gabriel Mukobi <gabrielmukobi@gmail.com>
2023-04-14 02:13:54 -0700

    Further improve sync loop

--- sync_wandb_loop.sh
+++ sync_wandb_loop.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-# Calculate the number of iterations for X days (X days * 24 hours/day)
-days = 4
-iterations = $(bc -l <<< "$days * 24")
 
-for i in $(seq 1 $iterations); do
-  echo "Syncing wandb run $i of $iterations ($(bc -l <<< "$i / 24") of $days days)"
-  sleep 1
-  # wandb sync wandb/run*
-  # sleep 3600  # Sleep for 1 hour (3600 seconds)
+# Set the number of days as a floating point variable
+DAYS=4.0
+
+# Calculate the number of iterations (hours)
+iterations=$(awk "BEGIN {print $DAYS * 24}")
+
+# Convert the float to an integer to use in the loop
+iterations_int=$(printf "%.0f" $iterations)
+
+# Loop for the calculated number of iterations
+for i in $(seq 1 $iterations_int); do
+  # Calculate the fractional days
+  fractional_days=$(awk "BEGIN {print $i / 24}")
+
+  # Print the progress
+  echo "Syncing wandb experiments... (iteration $i/$iterations_int, day $fractional_days/$DAYS)"
+
+  # Do the sync
+  wandb sync wandb/run*
+
+  # Sleep for 1 hour (3600 seconds)
+  sleep 3600
 done
