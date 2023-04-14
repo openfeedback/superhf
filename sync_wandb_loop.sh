74c028c74a7d563a7331068766bb49251f85ea16
Gabriel Mukobi <gabrielmukobi@gmail.com>
2023-04-14 02:05:08 -0700

    Improve sync script

--- sync_wandb_loop.sh
+++ sync_wandb_loop.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-for i in {1..16}; do
-  wandb sync wandb/run*
-  sleep 1800
+# Calculate the number of iterations for X days (X days * 24 hours/day)
+days = 4
+iterations = $(bc -l <<< "$days * 24")
+
+for i in $(seq 1 $iterations); do
+  echo "Syncing wandb run $i of $iterations ($(bc -l <<< "$i / 24") of $days days)"
+  sleep 1
+  # wandb sync wandb/run*
+  # sleep 3600  # Sleep for 1 hour (3600 seconds)
 done
