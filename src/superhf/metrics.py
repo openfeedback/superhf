Log KL then fix it!

src/superhf/metrics.py
@@ -27,6 +27,7 @@ class SuperHFMetrics:
     scores: list[float]
     filtered_scores: list[float]
     average_loss: float
+    average_kl_div: float
     scheduler_lr: float
     completion_lengths: list[int]
     filtered_completion_lengths: list[int]
@@ -54,7 +55,7 @@ def report_metrics_print(metrics: SuperHFMetrics) -> None:
         f" length {average_completion_length:.3f}, average filtered completion length"
         f" {average_filtered_completion_length:.3f}\naverage score {average_score:.3f},"
         f" average filtered score {average_filtered_score:.3f}, average loss"
-        f" {metrics.average_loss:.3f}."
+        f" {metrics.average_loss:.3f}, average KL {metrics.average_kl_div:.3f}."
     )
 
 
@@ -67,6 +68,7 @@ def initialize_metrics_wandb() -> None:
     """
     wandb.define_metric("average_loss", summary="min")
     wandb.define_metric("average_score", summary="max")
+    wandb.define_metric("average_score", summary="last")
     wandb.define_metric("average_completion_length", summary="last")
 
 
@@ -112,6 +114,7 @@ def report_metrics_wandb(metrics: SuperHFMetrics) -> None:
             "average_filtered_score": average_filtered_score,
             "filtered_score_histogram": wandb.Histogram(metrics.filtered_scores),
             "average_loss": metrics.average_loss,
+            "average_kl_div": metrics.average_kl_div,
             "scheduler_lr": metrics.scheduler_lr,
             "average_completion_length": np.mean(metrics.completion_lengths),
             "completion_length_histogram": wandb.Histogram(metrics.completion_lengths),

src/superhf/training.py
@@ -226,7 +226,7 @@ class SuperHFTrainer:
             )
 
             # Fine-tune the language model on the filtered completions
-            average_loss = find_executable_batch_size(
+            average_loss, average_kl_div = find_executable_batch_size(
                 self.finetune_language_model,
                 self.training_args.minibatch_size_finetuning,
             )(filtered_completions)
@@ -240,6 +240,7 @@ class SuperHFTrainer:
                 scores=scores,
                 filtered_scores=filtered_scores,
                 average_loss=average_loss,
+                average_kl_div=average_kl_div,
                 scheduler_lr=self.scheduler.get_last_lr()[0],
                 completion_lengths=completion_lengths,
                 filtered_completion_lengths=filtered_completion_lengths,
@@ -505,11 +506,11 @@ class SuperHFTrainer:
         self,
         minibatch_size: int,
         filtered_completions: list[str],
-    ) -> float:
+    ) -> tuple[float, float]:
         """
         Fine-tune the language model on the completions.
 
-        Returns the average loss for metrics. TODO also return the KL term.
+        Returns the average loss and KL divergence (or 0) for metrics.
         """
         # pylint: disable=too-many-locals
 
@@ -534,6 +535,7 @@ class SuperHFTrainer:
         num_invalid_losses = 0
         self.language_model.train()
 
+        sum_kl_divergence = 0
         for minibatch in tqdm(finetuning_dataloader, desc="Fine-tuning"):
             self.optimizer.zero_grad()
             outputs = self.language_model(**minibatch)
@@ -552,15 +554,18 @@ class SuperHFTrainer:
             # KL divergence penalty
             if self.training_args.kl_coefficient > 0:
                 # Assume there's only 1 sequence in the minibatch and we're using LoRA models
-                logp_online_model = outputs.logits[0]
+                logp_online_model = torch.log(torch.softmax(outputs.logits[0], dim=1))
 
                 # Disable LoRA adapters
                 with self.language_model.disable_adapter(), torch.no_grad():  # type: ignore
                     # Get the logits from the original model
                     logp_original_model = self.language_model(**minibatch)
-                    logp_original_model = logp_original_model.logits[0]
+                    logp_original_model = torch.log(
+                        torch.softmax(logp_original_model.logits[0], dim=1)
+                    )
 
                 kl_divergence = self.kl_loss(logp_online_model, logp_original_model)
+                sum_kl_divergence += kl_divergence.item()
                 loss = loss + self.training_args.kl_coefficient * kl_divergence
 
             sum_loss += loss.item()
@@ -576,7 +581,9 @@ class SuperHFTrainer:
             )
 
         num_valid_losses = len(finetuning_dataloader) - num_invalid_losses
-        return sum_loss / num_valid_losses if num_valid_losses > 0 else 0
+        return sum_loss / num_valid_losses if num_valid_losses > 0 else 0, (
+            sum_kl_divergence / num_valid_losses if num_valid_losses > 0 else 0
+        )
 
     def save_model(self) -> None:
         """