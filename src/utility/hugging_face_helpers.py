581a81fe846a45bb6bd94c82fac197ec6d96abaa
Peter Chatain <pchatain@stanford.edu>
2023-05-08 18:40:47 -0700

    code for changing the branch names on huggingface

---
+++ src/utility/hugging_face_helpers.py
@@ -0,0 +1,14 @@
+"""
+A file guide for performing hugging face operations
+"""
+
+# from huggingface_hub import create_branch
+
+# This creates a new branch from peterchatain/test-save-alpaca called
+#   model-2048-prompts-batch-8 from the commit 6e3528e2df24141a884c98ca7c8cb36a6bd0e2a9
+# create_branch("peterchatain/test-save-alpaca", repo_type="model",
+#   branch="model-8000-prompts-batch-8", revision="6e3528e2df24141a884c98ca7c8cb36a6bd0e2a9")
+# create_branch("peterchatain/test-save-alpaca", repo_type="model",
+#   branch="model-2048-prompts-batch-8", revision="07371ad76c45fff41f2860bb0825a2a4697d74de")#wrong
+# create_branch("peterchatain/test-save-alpaca", repo_type="model",
+#   branch="model-2048-prompts-batch-size-8", revision="5567969dc3859e6465f863fa86f2e6be9ed65553")
