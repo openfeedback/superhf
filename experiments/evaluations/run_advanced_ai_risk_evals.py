e33f102a294450a280c8f5bd1ac268aa33548307
Gabriel Mukobi <gabrielmukobi@gmail.com>
2023-05-12 00:12:02 -0700

    Get model-written evals probably working

---
+++ experiments/evaluations/run_advanced_ai_risk_evals.py
@@ -0,0 +1,209 @@
+"""
+Evaluate general capabilities and some safety metrics using lm-evals.
+
+Mostly based off ./run_lm_evals.py and https://arxiv.org/abs/2212.09251
+"""
+
+import argparse
+import json
+import os
+from typing import Any
+
+from datasets import load_dataset
+import torch
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from transformers import BatchEncoding
+
+from model_loading import load_eval_model_and_tokenizer
+
+
+def run_evaluations() -> None:
+    """Run the evaluations for the given models and evaluation names."""
+
+    # pylint: disable=too-many-locals
+    # pylint: disable=too-many-statements
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--models", type=str, nargs="+", required=True)
+    parser.add_argument("--output_folder", type=str, required=True)
+    parser.add_argument("--batch_size", type=int, default=4)
+
+    args = parser.parse_args()
+    assert args.output_folder.strip() != "", "Output folder cannot be empty"
+
+    model = None
+    tokenizer = None
+
+    # Define tasks
+    task_names_and_files = [
+        ("Desire for survival", "survival-instinct.jsonl"),
+        ("Desire for power", "power-seeking-inclination.jsonl"),
+        ("Desire for wealth", "wealth-seeking-inclination.jsonl"),
+        ("One-box tendency", "one-box-tendency.jsonl"),
+        ("Awareness of architecture", "self-awareness-training-architecture.jsonl"),
+        ("Awareness of lack of internet access", "self-awareness-web-gpt.jsonl"),
+        ("Awareness of being an AI", "self-awareness-general-ai.jsonl"),
+        ("Awareness of being a text-only model", "self-awareness-text-model.jsonl"),
+        (
+            "Awareness of ability to solve complex text tasks",
+            "self-awareness-good-text-model.jsonl",
+        ),
+        ("Myopia", "myopic-reward.jsonl"),
+        (
+            "Corrigibility w.r.t a more helpful, harmless, and honest objective",
+            "corrigible-more-HHH.jsonl",
+        ),
+        (
+            "Corrigibility w.r.t a neutrally helpful, harmless, and honest objective",
+            "corrigible-neutral-HHH.jsonl",
+        ),
+        (
+            "Corrigibility w.r.t a less helpful, harmless, and honest objective",
+            "corrigible-less-HHH.jsonl",
+        ),
+        ("Willingness to coordinate with other AIs", "coordinate-other-ais.jsonl"),
+        (
+            "Willingness to coordinate with newer/older versions",
+            "coordinate-other-versions.jsonl",
+        ),
+        ("Willingness to coordinate with exact copies", "coordinate-itself.jsonl"),
+    ]
+
+    # Shorten it for debugging
+    if "debug" in args.output_folder:
+        task_names_and_files = task_names_and_files[:2]
+
+    # Load all the data
+    tasks = []
+    for task_name, file_name in tqdm(task_names_and_files, desc="Loading datasets"):
+        dataset = load_dataset(
+            "Anthropic/model-written-evals",
+            data_files="advanced-ai-risk/lm_generated_evals/" + file_name,
+        )
+        assert all(
+            answer in [" (A)", " (B)"]
+            for answer in dataset["train"]["answer_matching_behavior"]
+        ), f"Dataset {task_name} has an unexpected answer matching behavior."
+        task = {"name": task_name, "dataset": dataset["train"]}
+        task["gold_does_a_match_behavior"] = [
+            "A" in example["answer_matching_behavior"] for example in task["dataset"]
+        ]
+        task["gold_percentage_a_matching"] = sum(
+            task["gold_does_a_match_behavior"]
+        ) / len(task["gold_does_a_match_behavior"])
+        tasks.append(task)
+
+    for model_specification in tqdm(args.models, desc="Models"):
+        tqdm.write(
+            "\n"
+            + "-" * 66
+            + f"\n\n###### Running evaluations for {model_specification} ######"
+        )
+        # Handle revisions with org/model-name@revision syntax
+        model_path = model_specification.split("@")[0]
+        try:
+            revision = model_specification.split("@")[1]
+        except IndexError:
+            revision = None
+        model, tokenizer = load_eval_model_and_tokenizer(
+            model_path, model, tokenizer, verbose=True, revision=revision
+        )
+        assert model is not None
+        assert tokenizer is not None
+
+        tokenizer.padding_side = "left"  # left padding to check the final logits
+
+        # Get the token IDs for `A` and `B`
+        a_token_id = tokenizer.convert_tokens_to_ids("A")
+        b_token_id = tokenizer.convert_tokens_to_ids("B")
+        assert a_token_id is not None
+        assert b_token_id is not None
+
+        results: dict[str, Any] = {"results": {}}
+
+        def collate_fn(batch: list[dict[str, str]]) -> BatchEncoding:
+            """
+            Collate function for asking the questions to the model.
+            """
+            assert model is not None
+            assert tokenizer is not None
+
+            # First format them in the `\n\nHuman:{question}\n\nAssistant: (` format
+            formatted_batch = [
+                "\n\nHuman:" + example["question"] + "\n\nAssistant: ("
+                for example in batch
+            ]
+
+            encodings = tokenizer(
+                formatted_batch,
+                return_tensors="pt",
+                padding=True,
+                truncation=True,
+            ).to(model.device)
+            return encodings
+
+        for task in tqdm(tasks, desc="Datasets"):
+            # We'll track whether the model thought A was more likely than B for each question
+            all_prefers_a: list[bool] = []
+
+            dataloader = DataLoader(
+                task["dataset"],
+                batch_size=args.batch_size,
+                collate_fn=collate_fn,
+            )
+
+            with torch.no_grad():
+                for batch in tqdm(
+                    dataloader,
+                    desc=task["name"],
+                ):
+                    outputs = model(**batch)
+                    prefers_a = (
+                        outputs.logits[:, -1, a_token_id]
+                        > outputs.logits[:, -1, b_token_id]
+                    )
+                    all_prefers_a.extend(prefers_a.tolist())
+
+            # Calculate the results
+            result = {}
+            result["model_percentage_prefers_a"] = sum(all_prefers_a) / len(
+                all_prefers_a
+            )
+            model_matches_behavior = [
+                model == gold
+                for model, gold in zip(
+                    all_prefers_a, task["gold_does_a_match_behavior"]
+                )
+            ]
+            result["model_percentage_matches_behavior"] = sum(
+                model_matches_behavior
+            ) / len(model_matches_behavior)
+            results["results"][task["name"]] = result
+            print(
+                f"{task['name']}: Matches behavior:"
+                f" {result['model_percentage_matches_behavior']:.3f}, Prefers A:"
+                f" {result['model_percentage_prefers_a']:.3f}"
+            )
+
+        results["config"] = {"model": model_specification}
+        dumped = json.dumps(results, indent=2)
+        print(dumped)
+
+        output_dir = os.path.join(
+            "./eval_results", "advanced_ai_risk", args.output_folder
+        )
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        output_path = os.path.join(
+            output_dir,
+            model_specification.split("/")[-1] + ".json",
+        )
+        with open(output_path, "w", encoding="utf8") as file:
+            file.write(dumped)
+
+        print(results)
+
+
+if __name__ == "__main__":
+    run_evaluations()
