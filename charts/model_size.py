639caccb52cf09958082fc2359ecb55ab262c18b
Gabriel Mukobi <gabrielmukobi@gmail.com>
2023-05-14 10:57:34 -0700

    Plot base pythia models

--- charts/model_size.py
+++ charts/model_size.py
@@ -32,27 +32,52 @@ def main() -> None:
         "shf-pythia-2.8b.json",
         "shf-pythia-6.9b.json",
         "shf-pythia-12b.json",
+        "pythia-70m-deduped.json",
+        "pythia-160m-deduped.json",
+        "pythia-410m-deduped.json",
+        "pythia-1b-deduped.json",
+        "pythia-1.4b-deduped.json",
+        "pythia-2.8b-deduped.json",
+        "pythia-6.9b-deduped.json",
+        "pythia-12b-deduped.json",
     ]
     parameters = [7e7, 1.6e8, 4.1e8, 1e9, 1.4e9, 2.8e9, 6.9e9, 1.2e10]
 
     # Calculate plot values for data
-    all_data = []
-    for parameter, file_name in zip(parameters, file_names):
+    shf_data = []
+    pretrained_data = []
+    for parameter, file_name in zip(parameters * 2, file_names):
         file_path = f"./experiments/evaluations/test_scores/{file_name}"
         scores = get_test_scores(file_path)
         named_scores = [[parameter, score] for score in scores]
-        all_data.extend(named_scores)
+        if "shf" in file_name:
+            shf_data.extend(named_scores)
+        else:
+            pretrained_data.extend(named_scores)
 
-    dataframe = pd.DataFrame(all_data, columns=["Parameter", "Score"])
+    dataframe_shf = pd.DataFrame(shf_data, columns=["Parameter", "Score"])
+    dataframe_pretrained = pd.DataFrame(pretrained_data, columns=["Parameter", "Score"])
 
     # Create the plot
     sns.lineplot(
-        data=dataframe,
+        data=dataframe_pretrained,
         x="Parameter",
         y="Score",
         errorbar="ci",
+        label="Pythia Base",
+        color=model_type_to_palette_color("pretrained"),
+    )
+    sns.lineplot(
+        data=dataframe_shf,
+        x="Parameter",
+        y="Score",
+        errorbar="ci",
+        label="SuperHF",
         color=model_type_to_palette_color("superhf"),
     )
+
+    # Legend
+    plt.legend(loc="upper left")
 
     # Set x-axis to log
     plt.xscale("log")
@@ -62,8 +87,8 @@ def main() -> None:
 
     # Set labels and title
     plt.xlabel("Parameters")
-    plt.ylabel("Test score")
-    plt.title("SuperHF scaling (Pythia model suite)")
+    plt.ylabel("Test Score")
+    plt.title("SuperHF Scaling (Pythia Model Suite)")
 
     # Save the plot
     save_plot(OUTPUT_FILE)
