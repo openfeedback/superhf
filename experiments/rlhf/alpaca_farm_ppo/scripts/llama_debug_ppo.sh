40147607470dd41a4ac1c32285b827b97c0a41d8
Peter Chatain <pchatain@stanford.edu>
2023-06-14 21:56:35 -0700

    attempt run through wtih mock for RM

--- experiments/rlhf/alpaca_farm_ppo/scripts/llama_debug_ppo.sh
+++ experiments/rlhf/alpaca_farm_ppo/scripts/llama_debug_ppo.sh
@@ -23,7 +23,7 @@ ${command} \
   --rollout_per_device_batch_size 1 \
   --per_device_eval_batch_size 1 \
   --output_dir "llama_output_alpaca_farm" \
-  --reward_model_name_or_path "/juice5/scr5/nlp/llama_model/llama_hf_latest/llama-7b" \
+  --reward_model_name_or_path "peterchatain/mock_llama" \
   --policy_model_name_or_path "/juice5/scr5/nlp/llama_model/llama_hf_latest/llama-7b" \
   --init_value_with_reward True \ # TODO: Change to false
   --rollout_batch_size 8 \
