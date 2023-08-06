783c79c15dafb7110c4344595bf3bf594c01e670
Gabriel Mukobi <gabrielmukobi@gmail.com>
2023-08-06 15:53:25 +0100

    Add HHH  to downstream

--- charts/downstream_evals.py
+++ charts/downstream_evals.py
@@ -13,6 +13,8 @@ from chart_utils import (
     initialize_plot,
     save_plot,
     load_json,
+    model_type_to_palette_color,
+    model_type_to_hatch,
     MODEL_NAME_MAPPING,
     QUALITATIVE_MODEL_ORDER,
 )
@@ -26,6 +28,8 @@ def main() -> None:
     """Main function."""
 
     # pylint: disable=too-many-locals
+    # pylint: disable=too-many-branches
+    # pylint: disable=too-many-statements
 
     # Initialize
     initialize_plot()
@@ -63,6 +67,28 @@ def main() -> None:
                     accuracies.append(result["acc"])
                     errors.append(result["acc_stderr"])
 
+            # Add HHH evals from https://github.com/openfeedback/mmlu_and_hhh_evals manually
+            if category_folder == "safety":
+                # We add the Harmless, Helpful, and Honest Average Scores for each
+                if model_name == "LLaMA":
+                    accuracies.extend([0.499, 0.529, 0.533])
+                elif model_name == "FeedME":
+                    accuracies.extend([0.488, 0.511, 0.533])
+                elif model_name == "Instruct":
+                    accuracies.extend([0.5, 0.528, 0.536])
+                elif model_name == "RLHF (LLaMA)":
+                    accuracies.extend([0.483, 0.531, 0.537])
+                elif model_name == "RLHF (Instruct)":
+                    accuracies.extend([0.486, 0.515, 0.527])
+                elif model_name == "SuperHF (LLaMA)":
+                    accuracies.extend([0.502, 0.524, 0.539])
+                elif model_name == "SuperHF (Instruct)":
+                    accuracies.extend([0.499, 0.540, 0.542])
+                elif model_name == "Alpaca":
+                    accuracies.extend([0.370, 0.589, 0.531])
+                else:
+                    raise ValueError(f"Unknown model {model_name}")
+
             average_acc = np.average(accuracies)
             average_err = np.average(errors)
             all_data.append((category_name, model_name, average_acc, average_err))
@@ -74,11 +100,17 @@ def main() -> None:
         all_data, columns=["Category", "Model", "Average", "Error"]
     )
 
+    # Add color and hatch columns
+    colors = [
+        model_type_to_palette_color(model) for model in QUALITATIVE_MODEL_ORDER
+    ] * 3
+
     # Reorder the model names by QUALITATIVE_MODEL_ORDER
     dataframe["Model"] = pd.Categorical(dataframe["Model"], QUALITATIVE_MODEL_ORDER)
 
     # Create the plot
     plt.rcParams["lines.markersize"] = 1  # No markers for bar plots
+
     barplot = sns.catplot(
         data=dataframe,
         kind="bar",
@@ -86,11 +118,19 @@ def main() -> None:
         y="Average",
         hue="Model",
         capsize=0.1,
+        palette=colors,
         # yerr=dataframe["Error"],
         # errorbar="ci",
         # order=QUALITATIVE_MODEL_ORDER,
         # label="Pythia Base",
     )
+
+    # Add hatches
+    hatches = [[model_type_to_hatch(model)] * 3 for model in QUALITATIVE_MODEL_ORDER]
+    hatches = [hatch for sublist in hatches for hatch in sublist]
+    for i, patch in enumerate(barplot.ax.patches):
+        patch.set_hatch(hatches[i])
+    # TODO Add hatching to legend colors too
 
     # Add error bars
     for i, model in enumerate(dataframe["Model"].unique().categories):
